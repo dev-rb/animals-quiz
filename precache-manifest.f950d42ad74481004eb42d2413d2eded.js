52083aab1c70352647dd7dd69457f0a26f401ae1
dev-rb <43100342+dev-rb@users.noreply.github.com>
2020-05-21 21:03:37 -0400

    Updates

---
+++ precache-manifest.f950d42ad74481004eb42d2413d2eded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4139e199d6aee0c6b82496f84d9883f",
+    "url": "/animals-quiz/index.html"
+  },
+  {
+    "revision": "5d030ee00f5f98885f88",
+    "url": "/animals-quiz/static/css/main.0f2747fb.chunk.css"
+  },
+  {
+    "revision": "5b96686ffad8ace831d7",
+    "url": "/animals-quiz/static/js/2.e450ba46.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/animals-quiz/static/js/2.e450ba46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d030ee00f5f98885f88",
+    "url": "/animals-quiz/static/js/main.5565f807.chunk.js"
+  },
+  {
+    "revision": "5a16f5fef9a46928f063",
+    "url": "/animals-quiz/static/js/runtime-main.6081a44b.js"
+  },
+  {
+    "revision": "ae7e059c005b71dd6c3b1a25437bb451",
+    "url": "/animals-quiz/static/media/bear2.ae7e059c.png"
+  },
+  {
+    "revision": "6093717f7ce5071717c888699d17f56f",
+    "url": "/animals-quiz/static/media/butterfly2.6093717f.png"
+  },
+  {
+    "revision": "efa2204e65a0400dbbdb0b1862ff13f0",
+    "url": "/animals-quiz/static/media/tiger2.efa2204e.png"
+  }
+]);
